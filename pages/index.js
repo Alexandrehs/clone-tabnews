c0f38c6e2aa105bb91ad1378a8170434596a1215
Alexandre Silva <echo.alexandre@gmail.com>
2024-08-18 17:16:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu presentinho de Jeová lhe amo trem lindo {'<3'}</h1>
+  return <h1>Meu presentinho de Jeová lhe amo trem lindo {"<3"}</h1>;
 }
 
 export default Home;
