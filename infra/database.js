e528404fa665a99bc7240ea521978113d16e7b7f
Alexandre Silva <echo.alexandre@gmail.com>
2024-08-23 18:24:20 +0000

    insert function verify envonrinment e set ssl

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: setSSL(),
   });
   try {
     await client.connect();
@@ -22,3 +23,7 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function setSSL() {
+  process.env.NODE_ENV === "production" ? true : false;
+}
