845bd549d683edb033aea14b4ab583c9fa00600a
Alexandre Silva <echo.alexandre@gmail.com>
2024-08-25 20:33:36 +0000

    config `.env.development` and crete file and test migration

---
+++ test/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,8 @@
+test("should return 200 in page migration", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+});
